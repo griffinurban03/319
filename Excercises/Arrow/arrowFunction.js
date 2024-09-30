188c679c9db8f8fb45659bcc1775a82679c4ffde
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-09-30 13:32:55 -0500

    timeout function

--- Excercises/Arrow/arrowFunction.js
+++ Excercises/Arrow/arrowFunction.js
@@ -7,3 +7,10 @@ const mult = (a,b) => a * b;
 const sub = (a,b) => a - b;
 
 const double = a => a * 2;
+
+
+console.log("Before...");
+setTimeout(()=>{
+    console.log("Inside timer");
+},5000);
+console.log("After...");
